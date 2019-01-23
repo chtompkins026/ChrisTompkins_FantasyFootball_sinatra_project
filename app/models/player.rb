ffa596ba02ca5af760eacbf27e3a61a9b3d9ed41
Chris Tompkins <chtompkins026@gmail.com>
2019-01-23 16:09:52 -0500

    Added new player and user validations: email for users and symbols for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,6 @@
 class Player < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :name, :team, :position, message: " can not be blank!"
   validates_uniqueness_of :name, scope: :user_id, message: " can not be a duplicate of someone already on your team"
   validates_format_of :name, with: /\A[A-Z\sa-z]+\z/, message: "of player must only have letters and spaces"
 end
